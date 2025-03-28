90e6b6fb76e3fe786d6696c4ebf47ebde67957b1
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-28 23:02:02 +0530

    `feat: added new sidebar groups and menu items`

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -3,6 +3,7 @@
 import {
   Sidebar,
   SidebarContent,
+  SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarHeader,
@@ -36,34 +37,70 @@ const items = [
     icon: CreditCard,
   },
 ];
+
+const projects = [
+  {
+    title: "Project 1",
+  },
+  {
+    title: "Project 2",
+  },
+  {
+    title: "Project 3",
+  },
+];
 export function AppSidebar() {
   const pathname = usePathname();
   return (
     <Sidebar collapsible="icon" variant="floating">
       <SidebarHeader>Logo</SidebarHeader>
+      {/* Contents */}
       <SidebarContent>
-        <SidebarGroupLabel>Application</SidebarGroupLabel>
-        <SidebarGroupContent>
-          <SidebarMenu>
-            {items.map((item) => {
-              return (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link
-                      href={item.url}
-                      className={
-                        pathname === item.url ? "!bg-primary !text-white" : ""
-                      }
-                    >
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              );
-            })}
-          </SidebarMenu>
-        </SidebarGroupContent>
+        {/* Group 1 */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => {
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.url}
+                        className={
+                          pathname === item.url ? "!bg-primary !text-white" : ""
+                        }
+                      >
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* Group 2 */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Your Projects</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {projects.map((project) => {
+                return (
+                  <SidebarMenuItem key={project.title}>
+                    <SidebarMenuButton asChild>
+                      <div>
+                        <div className="rounded-sm border size-6 flex items-center justify-center text-sm bg-white text-primary"></div>
+                      </div>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
     </Sidebar>
   );
