3dc9ee84eae95db13d9e4b02d7bc76588aee2b61
Anjany Pandey <anjany.pandey06@gmail.com>
2025-03-28 19:15:23 +0530

    setup clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-red-600">Hey</h1>
+      <Button>hey</Button>
     </div>
   );
 }
